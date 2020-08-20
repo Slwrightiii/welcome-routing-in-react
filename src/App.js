e7fe4c9fd7df6dad65d0a34beaf73487727d4146
Samuel Wright III <trey1991chad@yahoo.com>
2020-08-20 19:49:04 -0400

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { Redirect, Switch, BrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Navigation from "./components/navigation/Navigation";
+import Contact from "./components/contact/Contact";
+import Notfound from "./components/notfound/Notfound";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* define our routes */}
+
+        <Navigation />
+
+        <Switch>
+          <Route path="/clock" component={Clock} />
+          <Route path="/contact" component={Contact} />
+
+          <Route
+            exact
+            path="/"
+            render={(props) => <Welcome {...props} name="eric" />}
+          />
+
+          <Route
+            exact
+            path="/welcome/:name"
+            render={(props) => <Welcome {...props} />}
+          />
+
+          <BrowserRouter>
+            <Route exact path="/404" component={Notfound} />
+
+            <Redirect to="/404" />
+          </BrowserRouter>
+        </Switch>
+      </div>
+    );
+  }
 }
-
 export default App;
